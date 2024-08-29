c5bb36f1b86913b1a52e8650f9f5ae96f169c4ee
Sofía González <sofia_1.6@hotmail.com>
2024-08-29 19:07:26 -0300

    CLI Resource y Seed endpoint

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -8,21 +8,11 @@ import { Car } from './interfaces/car.interface';
 @Injectable()
 export class CarsService {
   private cars: Car[] = [
-    {
-      id: uuid(),
-      brand: 'Toyota',
-      model: 'Corolla',
-    },
-    {
-      id: uuid(),
-      brand: 'Honda',
-      model: 'Civic',
-    },
-    {
-      id: uuid(),
-      brand: 'Jeep',
-      model: 'Cherokee',
-    },
+    // {
+    //   id: uuid(),
+    //   brand: 'Toyota',
+    //   model: 'Corolla',
+    // },
   ];
 
   findAll() {
@@ -73,5 +63,9 @@ export class CarsService {
   delete ( id: string ) {
     const car = this.findOneById( id );
     this.cars = this.cars.filter( car => car.id !== id );
+  }
+
+  fillCarsWithSeedData( cars: Car[] ) {
+    this.cars = cars;
   }
 }
---
+++ src/seed/seed.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { BrandsService } from 'src/brands/brands.service';
+import { CarsService } from '../cars/cars.service'; 
+
+import { CARS_SEED } from './data/cars.seed';
+import { BRANDS_SEED } from './data/brands.seed';
+
+
+
+@Injectable()
+export class SeedService {
+
+  constructor(
+    private readonly carsService: CarsService,
+    private readonly brandsService: BrandsService
+  ) {}
+
+
+  populateDB() {
+    this.carsService.fillCarsWithSeedData( CARS_SEED );
+    this.brandsService.fillBrandsWithSeedData( BRANDS_SEED );
+
+    return 'SEED executed';
+  }
+}
